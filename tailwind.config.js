832b7f036cbec4e0014ed915a8c23841fd9b63fd
AtharvaNair04 <atharvanair04@gmail.com>
2025-04-16 00:48:42 +0530

    Setting up the basics

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./app/**/*.{js,ts,jsx,tsx,mdx}",
-      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-      "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-      extend: {},
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary-yellow': '#FEC430',
+        'dark-bg': '#1E1E1E',
+        'light-grey-bg': 'rgba(217, 217, 217, 0.02)',
+        'white-text': '#FFFFFF',
+        'red': '#FF5252',
+        'dark-red': '#B20503',
+        'green': '#00FF09',
+        'grey': '#777777',
+        'muted-grey': '#C7C7C7',
+        'deep-grey': '#464646',
+        'dark-grey': '#303030',
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
