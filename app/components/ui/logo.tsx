496163b85399571ddf31575bd7a029b7c3de3699
AtharvaNair04 <atharvanair04@gmail.com>
2025-04-16 12:10:21 +0530

    Aded logo

---
+++ app/components/ui/logo.tsx
@@ -0,0 +1,15 @@
+'use client';
+import Link from 'next/link';
+
+const Logo = ({ href = '/dashboard' }) => {
+  return (
+    <Link href={href} className="text-white-text text-xl font-bold">
+      <span>amMENT</span>
+      <span className="text-primary-yellow">&lt;</span>
+      <span className="text-primary-yellow">&gt;</span>
+      <span>R</span>
+    </Link>
+  );
+};
+
+export default Logo;
