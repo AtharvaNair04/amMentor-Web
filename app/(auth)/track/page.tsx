644063aa42e01fd4146bff74bf484e2a6dc55390
PinJinx <rohithkpradeep@gmail.com>
2025-05-21 18:08:07 +0530

    track integration

--- app/(auth)/track/page.tsx
+++ app/(auth)/track/page.tsx
@@ -4,17 +4,30 @@ import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Brain, Code, Smartphone, Monitor, Award } from 'lucide-react';
 
+
+
+
 export default function TrackSelectionPage() {
   const [selectedTrack, setSelectedTrack] = useState<string>('');
-  
-  const tracks = [
-    { id: 'ai', name: 'AI', icon: Brain },
-    { id: 'web', name: 'Web Development', icon: Code },
-    { id: 'mobile', name: 'Mobile Development', icon: Smartphone },
-    { id: 'systems', name: 'Systems', icon: Monitor },
-    { id: 'vidyaratna', name: 'Vidyaratna', icon: Award },
-  ];
 
+  const [tracks, updatetracks] = useState<{ id: number; name: string; icon: React.ElementType }[]>([]);
+
+
+  async function fetchdata() {
+    const icons_set = {1:Brain,2:Code,3:Smartphone,4:Monitor,5:Award};//Icons wrt ids
+    const data = await fetch("https://ammentor.up.railway.app/tracks/");    
+    const response: { id: number; title: string }[] = await data.json();
+
+    const updatedTracks = response.map((element) => ({
+      id: element["id"],
+      name: element["title"],
+      icon: icons_set[element.id as keyof typeof icons_set],
+    }));
+    updatetracks(updatedTracks);
+  }
+
+
+  fetchdata();
   const router = useRouter();
 
   const handleTrackSelect = (trackId: string) => {
@@ -33,7 +46,7 @@ export default function TrackSelectionPage() {
             <button
               key={track.id}
               onClick={() => handleTrackSelect(track.id)}
-              className={`flex justify-start items-center w-full pl-5 px-24 py-2 rounded-3xl shadow-lg transition-colors duration-200 ${
+              className={`flex justify-start items-center  w-full pl-5 px-24 py-2 rounded-3xl shadow-lg transition-colors duration-200 ${
                 selectedTrack === track.id 
                   ? 'bg-[#464646] text-yellow-400' 
                   : 'bg-[#464646] text-yellow-400 hover:bg-yellow-400 hover:text-black'
