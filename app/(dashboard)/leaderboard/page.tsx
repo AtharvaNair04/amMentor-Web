d24054308c2a055acc56aba435fab8c5ddfbad2f
AtharvaNair04 <atharvanair04@gmail.com>
2025-07-17 12:03:08 +0530

    Fix leaderboard interval

--- app/(dashboard)/leaderboard/page.tsx
+++ app/(dashboard)/leaderboard/page.tsx
@@ -41,7 +41,7 @@ const LeaderBoardPage = () => {
     ));
   };
 
-  const fetchLeaderboardData = async (currentTrackId: number) => {
+  const fetchLeaderboardData = useCallback(async (currentTrackId: number) => {
     try {
       const data = await fetchPlayerdata(currentTrackId);
       setLeaderboardData(data);
@@ -50,7 +50,7 @@ const LeaderBoardPage = () => {
     } finally {
       setLoading(false);
     }
-  };
+  }, []);
 
   const fetchOverallLeaderboard = useCallback(async () => {
     try {
@@ -94,10 +94,10 @@ const LeaderBoardPage = () => {
     }
   }, [tracks]);
 
-  const fetchTracksData = async () => {
+  const fetchTracksData = useCallback(async () => {
     const data = await fetchtrack();
     setTracks(data);
-  };
+  }, []);
 
   const handleTrackChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const value = event.target.value;
@@ -108,33 +108,35 @@ const LeaderBoardPage = () => {
     }
   };
 
-useEffect(() => {
-  if (!isLoggedIn) {
-    router.push('/');
-    return;
-  }
-  let interval: NodeJS.Timeout;
-  const runFetch = () => {
-    if (trackId === 'overall') {
-      if (tracks.length > 0) {
-        fetchOverallLeaderboard();
-      }
-    } else {
-      fetchLeaderboardData(trackId as number);
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.push('/');
+      return;
     }
-  };
-  setLoading(true);
-  runFetch();
-  interval = setInterval(() => {
+    
+    const runFetch = () => {
+      if (trackId === 'overall') {
+        if (tracks.length > 0) {
+          fetchOverallLeaderboard();
+        }
+      } else {
+        fetchLeaderboardData(trackId as number);
+      }
+    };
+    
+    setLoading(true);
     runFetch();
-  }, 10000);
-  return () => clearInterval(interval);
-}, [isLoggedIn, router, trackId, tracks, fetchOverallLeaderboard]);
-
+    
+    const interval = setInterval(() => {
+      runFetch();
+    }, 10000);
+    
+    return () => clearInterval(interval);
+  }, [isLoggedIn, router, trackId, tracks, fetchOverallLeaderboard, fetchLeaderboardData]);
 
   useEffect(() => {
     fetchTracksData();
-  }, []);
+  }, [fetchTracksData]);
 
   if (!isLoggedIn) {
     return null;
